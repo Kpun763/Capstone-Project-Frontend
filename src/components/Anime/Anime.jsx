e36702aebdaad785413471bfbfde91ea195e68c0
Kyle Washington <kmw1993@msn.com>
2023-10-19 15:35:17 -0400

    Added react import to my src and added openssl to the start in my package.json

--- src/components/Anime/Anime.jsx
+++ src/components/Anime/Anime.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 const Anime = ({id, title, pictureUrl}) => {
     return (
         <div>
--- src/pages/SearchResults/SearchResults.jsx
+++ src/pages/SearchResults/SearchResults.jsx
@@ -1,5 +1,5 @@
-import { useSearchParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import axios from "axios";
 
 const SearchResults = ({}) => {
